fce76a3d8ef9fce3a45003e077525cec797f2834
asliyaputri3 <asliyaputri3@gmail.com>
2026-02-20 06:05:46 +0700

    BookingService

--- src/controller/BookingController.java
+++ src/controller/BookingController.java
@@ -1,5 +1,27 @@
 package controller;
+import model.Customer;
+import model.Schedule;
+import service.BookingService;
 
 public class BookingController {
     // nanti untuk alur booking
+    private BookingService bookingService;
+
+    public BookingController(BookingService bookingService){
+        this.bookingService = bookingService;
+    }
+
+    public boolean pesanTiket(Customer customer, Schedule schedule, String seat, int harga){
+
+        boolean berhasil = bookingService.pesanTiket(customer, schedule, seat, harga);
+
+        if(!berhasil){
+            System.out.println("Gagal memesan tiket!");
+            return false;
+        }
+
+        System.out.println("Tiket berhasil dipesan!");
+        return true;
+    }
 }
+
--- src/service/BookingService.java
+++ src/service/BookingService.java
@@ -1,5 +1,53 @@
 package service;
+import repository.TikeRepository;
+import model.Schedule;
+import model.Tiket;
+import model.Customer;
 
 public class BookingService {
     //logic nanti apakah kursi sudah dipesan atau belum
+    private TikeRepository tiketRepo;
+
+    public BookingService(TikeRepository tiketRepo){
+        this.tiketRepo = tiketRepo;
+    }
+
+    public boolean isSeatAvailable(Schedule schedule, String seat){
+        for(Tiket t : tiketRepo.getAllTiket()){
+            if(t.getJadwalFilm().equals(schedule) &&
+               t.getSeat().equalsIgnoreCase(seat)){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public boolean pesanTiket(Customer customer, Schedule schedule, String seat, int harga){
+
+        
+        if(!isSeatAvailable(schedule, seat)){
+            return false;
+        }
+
+        
+        if(customer.getSaldo() < harga){
+            return false;
+        }
+
+        
+        customer.setSaldo(customer.getSaldo() - harga);
+
+        
+        Tiket tiketBaru = new Tiket(
+                tiketRepo.getAllTiket().size() + 1,
+                customer,
+                schedule,
+                seat,
+                harga
+        );
+
+        tiketRepo.addTiket(tiketBaru);
+
+        return true;
+    }
 }
